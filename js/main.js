021d533e11cb54b43197f98a831ddc0ffc43931c
acatella <aficclark@gmail.com>
2016-01-30 18:57:30 -0800

    did basic requirements

--- js/main.js
+++ js/main.js
@@ -13,35 +13,40 @@ $( document ).ready(function() {
         greatest: 0,
         total: 0
     };
-    // Place all your Javascript code inside this "document ready" function so
-    // it does not run until the DOM is ready for Javascript manipulation.
 
-    // TODO: Create a function to listen for clicks on the "login" button.
-    //      1. When a user clicks the "login" button, hide the login
-    //          form elements on the page.
-    //      2. Fill the user's first and last name into `div.user-info`.
-    //      (NOTE: You do not have to perform any validation on the data as
-    //          a base requirement.)
+    //Change login to user info on click event
+    $("#login-btn").click(function() {
+      //this needs to be more elegant
+        //can you use moveUp or moveDown?
+      $(".navbar-form").hide();
+      $(".user-fullname").text(userInfo.firstName + " " + userInfo.lastName);
+      $(".user-info").show();
+    });
 
+    //Change back to login for by clicking logout button
+    $("#js-logout-btn").click(function() {
+      $(".navbar-form").show();
+      $(".user-info").hide();
+    });
 
-    // TODO: Create a function to listen for clicks on all the "View Details"
-    // buttons so that when a user clicks a "View Details" button they see
-    // the content contained in the elements with the class "details" in the
-    // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
-    //      2. Within that parent, find all the elements that have the class `details`.
-    //      3. Toggle visibility of all the elements within that parent with the class `details`.
-    //      4. Change the text of the "view details" button to read "hide details" so the user
-    //          understands they can hide the text again.
+    //Hide and show details, update text of buttons accordingly
+    $(".view-details").click(function() {
+      var parent = $(this).parent().parent();
+      $(parent).children(".details").toggle();
+      $(this).text() === "View details »" ? $(this).text("Hide details »") : $(this).text("View details »");
+    });
 
-    // TODO: Create a function that listens for clicks on the voting buttons and
-    // looks at the `data-vote` attribute on each button to see what was voted for,
-    // then determines the updated vote breakdown to adjust the progress bars.
-    //      1. Set up an event listener on the buttons with the `vote` class.
-    //      2. When a button is clicked, look at the `data-vote` attribute to determine
-    //          what the user is voting for ("great" or "greatest").
-    //      3. Increment the counter for whichever vote talley is affected.
-    //      4. Determine the respective percentages (out of 100) for each progress bar.
-    //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+    //Add to vote counts and update bars
+    $('.vote').click(function(event) {
+      var button = $(event.target);
+      var dataVote = button.data('vote');
+      voteCounts[dataVote] += 1;
+      voteCounts.total += 1;      
+      updateBars();
+    });
 
+    function updateBars() {
+      $(".great-progress").css("width", (voteCounts.great/voteCounts.total) * 100 + "%");
+      $(".greatest-progress").css("width", (voteCounts.greatest/voteCounts.total) * 100+ "%");
+    };
 });
